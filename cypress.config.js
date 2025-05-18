7bd0638431e8df5c8df34ded48abad6826927126
yueshu <1256889228@qq.com>
2025-05-18 20:02:32 +0800

    ctpress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000', // 设置你的应用地址
+    baseUrl: 'http://localhost:5001', // 设置你的应用地址
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // 测试文件路径
     supportFile: false, // 可选：暂时不使用支持文件
   },
