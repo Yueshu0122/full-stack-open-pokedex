b9b71845a1eb222d45f13d105fbf4a42dccb39e1
yueshu <1256889228@qq.com>
2025-05-18 20:38:28 +0800

    gl

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,11 @@ module.exports = {
     'es6': true,
     'jest/globals': true,
     'node': true,
-    'globals': {
+  },
+  'globals': {
       'cy': true,     // 支持 cy 命令
       'Cypress': true // 支持 Cypress 对象（比如 Cypress.env()）
-    }
-
-  },
+    },
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended'
