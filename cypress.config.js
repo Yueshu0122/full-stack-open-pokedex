9edc278f815fd219f207251f91a7aa98a7c10ba3
yueshu <1256889228@qq.com>
2025-05-18 20:07:16 +0800

    cy

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:5001', // 设置你的应用地址
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // 测试文件路径
     supportFile: false, // 可选：暂时不使用支持文件
+    pageLoadTimeout: 120000,
   },
 })
