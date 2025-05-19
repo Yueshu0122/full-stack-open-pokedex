c27a3b101f15f0109af284d3a3aa1e0f8900d189
yueshu <1256889228@qq.com>
2025-05-19 20:19:51 +0800

    host

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`server started on port ${PORT}`)
 })
